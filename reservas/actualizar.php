6401316ea1f00a6f97efb8fe63ec8c71264e2511
Daniel Caparros <danicaparros97@gmail.com>
2019-01-02 20:38:09 +0100

    Ahora salen todos los datos que deben salir
    Hay que intentar que muestre la fecha, no se porque no la muestra. Si no se consigue no pasa nada porque funciona perfectamente

--- reservas/actualizar.php
+++ reservas/actualizar.php
@@ -1,5 +1,5 @@
 <?php
-spl_autoload_register(function ( $NombreClase) {
+spl_autoload_register(function ($NombreClase) {
     include_once($NombreClase . '.php');
 });
 include "./includes/cabecera.php";
@@ -9,40 +9,37 @@ include "./includes/cabecera.php";
     <h1>Actualizar reserva</h1>
 </header>
 <section>
-
     <?php
     if (isset($_GET["id"])) {
         $unaReserva = CrudReserva::mostrarID($_GET['id']);
-        echo $unaReserva->getId();
-        echo $unaReserva->getFecha();
     }
     ?>
     <form action="manager.php" method="post" enctype="multipart/form-data">
         <section>
+            <label>Apellidos:</label>
+            <input type="text" name="apellidos" value="<?php echo $unaReserva->getApellidos() ?>" readonly>
+        </section>
+        <section>
+            <label>Nombre:</label>
+            <input type="text" name="nombre" value="<?php echo $unaReserva->getNombre() ?>" readonly>
+        </section>
+        <section>
             <label>Fecha:</label>
-            <input type="text" name="fecha" required>
+            <input type="date" name="fecha" value="<?php echo $unaReserva->getFecha() ?>" required>
         </section>
         <section>
             <label>Hora:</label>
-            <input type="text" name="hora" required>
+            <input type="time" name="hora" value="<?php echo $unaReserva->getHora() ?>" required>
         </section>
         <section>
             <label>Comensales:</label>
-            <input type="text" name="comensales" required>
+            <input type="number" name="comensales" value="<?php echo $unaReserva->getComensales() ?>" required>
         </section>
         <section>
-            <input type="hidden" name="id" value="<?php echo $unaReserva->getId() ?>">
+            <input type="hidden" name="id" value="<?php echo $unaReserva->getId()?>">
         </section>
         <section>
-
-            <input type="hidden" name="apellidos" value="<?php echo $unaReserva->getApellidos() ?>">
-        </section>
-        <section>
-
-            <input type="hidden" name="nombre" value="<?php echo $unaReserva->getNombre() ?>">
-        </section>
-        <label></label>
-        <input type="submit" name="actualizar" value="Enviar">
+            <input type="submit" name="actualizar" value="Enviar">
         </section>
     </form>
 </section>
