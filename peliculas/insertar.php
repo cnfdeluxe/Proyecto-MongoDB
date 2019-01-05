527c602a1f9ed88fae0aa11a709edf863189c5f8
Cristian Navarro Fernandez <cnfnavarro@gmail.com>
2019-01-05 22:24:35 +0100

    subir fichero insertar

---
+++ peliculas/insertar.php
@@ -0,0 +1,62 @@
+<?php
+include "cabecera.php";
+?>
+
+<header class="cabecera">
+	<h1 class="text-center">Insertar pelicula</h1>
+</header>
+
+<?php
+	if(!$_POST){
+	 	//Si no has mandado nada pintas el formulario
+?>
+<section class=" row justify-content-md-center">
+    
+		<form action="manager.php" class="form-insertar" method="post" enctype="multipart/form-data">
+		 <ul class="form-insert">
+		 	<li>
+    			<label>Titulo:</label>
+    			<input type="text" class="form-control" name="titulo" maxlength="100" required>
+			</li>
+			<li>
+    			<label>Genero:</label>
+    			<input type="text" class="form-control" name="genero" maxlength="100" required>
+			</li>
+			<li>
+    			<label>Director:</label>
+    			<input type="text" class="form-control" name="director" required>
+			</li>
+			<li>
+    			<label>Año:</label>
+    			<input type="number" class="form-control" name="year" required>
+    		</li>
+   			<li>
+    			<label>Sinopsis:</label>
+    			<textarea name="sinopsis" class="form-control" id="" cols="40" rows="5"></textarea>
+    		</li>
+   			<li>
+    			<label>Portada:</label>
+    			<input type="file" name="portada" required>
+    		</li>    		
+			<li>
+    			<input type="submit" class="btn btn-primary" name="insertar" value="Enviar">
+			</li>
+        </ul>
+
+		</form>
+
+           
+</section>
+ <a href="index.php" class="back">
+                <button class="btn btn-primary">
+                    <i class="fas fa-reply"></i>
+                    Volver
+                </button>                
+            </a>
+
+<?php
+
+}
+
+include "pie.php";
+?>
